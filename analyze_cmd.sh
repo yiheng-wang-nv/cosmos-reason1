d35063aa4d1a301323c112435f2dcc2ccc8e151e
Yiheng Wang <vennw@nvidia.com>
2025-06-19 02:51:28 +0000

    rename
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- analyze_cmd.sh
+++
@@ -1,9 +0,0 @@
-# Set your server IP here
-SERVER_IP="0.0.0.0"
-
-python surgical_env_analyze.py debug_images/demo_input.png \
---server http://${SERVER_IP}:8000 \
---system-file system_message_analyze.txt \
---message "$(cat user_message_analyze.txt)" \
---max-tokens 1024 \
---temperature 0.0
