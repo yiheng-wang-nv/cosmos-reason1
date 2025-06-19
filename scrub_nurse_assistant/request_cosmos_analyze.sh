bfb9460e1d0bbcfa282c87b413089ed1ff75e526
Yiheng Wang <vennw@nvidia.com>
2025-06-19 20:49:44 +0800

    separate different method
    Signed-off-by: Yiheng Wang <vennw@nvidia.com>

--- scrub_nurse_assistant/request_cosmos_analyze.sh
+++ scrub_nurse_assistant/request_cosmos_analyze.sh
@@ -2,8 +2,8 @@
 # Set your server IP here
 SERVER_IP="10.176.228.194"
 
-# # Direct capture
-# python env_analyze.py --camera 2 \
+# # camera direct capture
+# python camera_image_analyze.py --camera 2 \
 # --server http://${SERVER_IP}:8000 \
 # --system-file system_message.txt \
 # --message "$(cat user_message.txt)" \
@@ -11,7 +11,7 @@ SERVER_IP="10.176.228.194"
 # --temperature 0.0
 
 # local image
-python env_analyze.py images/demo_input.png \
+python local_image_analyze.py images/demo_input.png \
 --server http://${SERVER_IP}:8000 \
 --system-file system_message.txt \
 --message "$(cat user_message.txt)" \
